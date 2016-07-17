e3074b3604d2250b34ebf50dbd8aa5bd410b6319
Ross Wardrup <minorsecond@gmail.com>
2016-07-17 08:31:08 -0500

    Added code to catch empty text inputs in origin calculator

--- gh.py
+++ gh.py
@@ -13,6 +13,8 @@ Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
 
 class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    entries_valid = False
+
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
@@ -26,15 +28,26 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         :return: Origin
         """
 
+        blank_entry = False
+
         # Grab values from text entry boxes and convert to floats
         # TODO: Data validation
-        north_y = float(self.northYEntry.text())
-        south_y = float(self.southYEntry.text())
-        east_x = float(self.eastXEntry.text())
-        west_x = float(self.westXEntry.text())
+        north_y = self.northYEntry.text()
+        south_y = self.southYEntry.text()
+        east_x = self.eastXEntry.text()
+        west_x = self.westXEntry.text()
 
-        print(north_y)
+        coordinates = (north_y, south_y, east_x, west_x)
 
+        for i in coordinates:
+            if len(i) == 1:
+                blank_entry = True
+
+        if not blank_entry:
+            north_y = float(coordinates[0])
+            south_y = float(coordinates[1])
+            east_x = float(coordinates[2])
+            west_x = float(coordinates[3])
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
