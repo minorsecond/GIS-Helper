3d5f80002a283f26453b5433227b5b40207c0dea
Ross Wardrup <minorsecond@gmail.com>
2016-07-17 13:48:41 -0500

    Added error catching for invalid characters in centroid calculation input boxes.

--- gh.py
+++ gh.py
@@ -62,18 +62,25 @@ class gishelper(QtWidgets.QMainWindow, Ui_MainWindow):
             info = "Check that all coordinate fields contain valid values."
             self.error_popup(title, text, info)
         else:
-            coordinates[0] = float(coordinates[0])
-            coordinates[1] = float(coordinates[1])
-            coordinates[2] = float(coordinates[2])
-            coordinates[3] = float(coordinates[3])
 
-            x = [coordinates[2], coordinates[3]]
-            y = [coordinates[0], coordinates[1]]
+            try:
+                coordinates[0] = float(coordinates[0])
+                coordinates[1] = float(coordinates[1])
+                coordinates[2] = float(coordinates[2])
+                coordinates[3] = float(coordinates[3])
 
-            centroid = (sum(x) / len(coordinates), sum(y) / len(coordinates))
-            centroid = "{0}, {1}".format(centroid[0], centroid[1])
+                x = [coordinates[2], coordinates[3]]
+                y = [coordinates[0], coordinates[1]]
 
-            outputText.setText(centroid)
+                centroid = (sum(x) / len(coordinates), sum(y) / len(coordinates))
+                centroid = "{0}, {1}".format(centroid[0], centroid[1])
+
+                outputText.setText(centroid)
+
+            except ValueError:
+                self.error_popup('Error', 'Error converting coordinates to decimal numbers.', 'Check to ensure '
+                                                                                              'coordinate input boxes '
+                                                                                              'contain only numbers.')
 
     def clear_origin_fields(self):
         self.northYEntry.clear()
