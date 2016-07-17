af878e11a2012e6dbf2df9d011328d0ecc7b91e9
Ross Wardrup <minorsecond@gmail.com>
2016-07-17 07:54:17 -0500

    Add calculate button to Python code.

--- gh.py
+++ gh.py
@@ -18,6 +18,23 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
 
+        self.originCalculateButton.clicked.connect(self.calculateOrigin)
+
+    def calculateOrigin(self):
+        """
+        Calculates the origin, given bounding box coordinates
+        :return: Origin
+        """
+
+        # Grab values from text entry boxes and convert to floats
+        # TODO: Data validation
+        north_y = float(self.northYEntry.text())
+        south_y = float(self.southYEntry.text())
+        east_x = float(self.eastXEntry.text())
+        west_x = float(self.westXEntry.text())
+
+        print(north_y)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
