bc0d97bd49834a1c64a0dc1bc861e13dc56f93eb
Ross Wardrup <minorsecond@gmail.com>
2016-07-17 07:11:29 -0500

    First commit - add all files.

---
+++ gh.py
@@ -0,0 +1,26 @@
+"""
+Tools to help with various GIS tasks
+Robert Ross Wardrup
+"""
+
+import sys
+
+from PyQt5 import uic, QtWidgets
+
+qtCreatorFile = "gisHelperGui.ui"  # Enter file here.
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
