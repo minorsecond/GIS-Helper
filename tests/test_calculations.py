60ac5eb099868fa40474a12f86fd23caa1d9874c
Ross Wardrup <minorsecond@gmail.com>
2016-07-21 17:11:06 -0500

    Fixed unit testing.

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -4,14 +4,15 @@ from gh import *
 def test_dd_to_dms():
     print(dd_to_dms(38.898556))
     print(dd_to_dms(-77.037852))
-    assert dd_to_dms(38.898556) == (38, 53, 54.802)
-    assert dd_to_dms(-77.037852) == (-77, -2, 16.267)
-    assert dd_to_dms(180.001) == False
+    print(dd_to_dms(23292))
+    assert dd_to_dms(38.898556) == (38, 53, 54.802, True)
+    assert dd_to_dms(-77.037852) == (-77, -2, 16.267, True)
+    assert dd_to_dms(180.001) == (None, None, None, False)
 
 
 def test_origin_calculation():
-    coordinates = [43.7291, 26.2946, 43.8058, 26.3759]
-    assert origin_calc(coordinates) == '43.76745, 26.33525'
+    coordinates = [0.0, 10.0, 0.0, 10.0]
+    assert origin_calc(coordinates) == (5.0, 5.0)
 
 if __name__ == '__main__':
-    test_dd_to_dms()
+    test_origin_calculation()
