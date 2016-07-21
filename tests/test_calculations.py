4e936434f400070cf9b2ed89bf0f9a131a1472fd
Robert Ross Wardrup <minorsecond@gmail.com>
2016-07-21 12:36:41 -0500

    Update and rename test_dd_to_dms.py to test_calculations.py

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -5,3 +5,7 @@ def test_dd_to_dms():
 	assert GisHelper.dd_to_dms(38.898556) == '38d, 53m, 55s'
 	assert GisHelper.dd_to_dms(-77.037852) == '77d, 2m, 16s'
 	assert GisHelper.dd_to_dms(100.123456) == 'Invalid entry'
+
+def test_origin_calculation():
+	coordinates = [(43.7291, 26.2946), (43.8058, 26.3759)]
+	assert GisHelper.calculate_origin(coordinates) == '43.76745, 26.33525'
