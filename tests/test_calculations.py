1c14df1835ba048ebcd8e39a55a2ca34cd002202
Ross Wardrup <minorsecond@gmail.com>
2016-07-21 17:11:46 -0500

    Cleaned up unit tests

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -2,9 +2,6 @@ from gh import *
 
 
 def test_dd_to_dms():
-    print(dd_to_dms(38.898556))
-    print(dd_to_dms(-77.037852))
-    print(dd_to_dms(23292))
     assert dd_to_dms(38.898556) == (38, 53, 54.802, True)
     assert dd_to_dms(-77.037852) == (-77, -2, 16.267, True)
     assert dd_to_dms(180.001) == (None, None, None, False)
