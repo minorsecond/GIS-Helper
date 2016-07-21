102aea9264e2dd9ba1601bb78c5f219f7865983c
Robert Ross Wardrup <minorsecond@gmail.com>
2016-07-21 12:21:52 -0500

    Update test_dd_to_dms.py

--- tests/test_dd_to_dms.py
+++ tests/test_dd_to_dms.py
@@ -1,5 +1,7 @@
-from gh.py import GisHelper.dd_to_dms
+from gh.py import GisHelper
+GisHelper = GisHelper()
+
 def test_dd_to_dms():
-	assert dd_to_dms(38.898556) == '38d, 53m, 55s'
-	assert dd_to_dms(-77.037852) == '77d, 2m, 16s'
-	assert dd_to_dms(100.123456) == 'Invalid entry'
+	assert GisHelper.dd_to_dms(38.898556) == '38d, 53m, 55s'
+	assert GisHelper.dd_to_dms(-77.037852) == '77d, 2m, 16s'
+	assert GisHelper.dd_to_dms(100.123456) == 'Invalid entry'
