b2a43ec52781f50c116e05aeba5b3fb573739cea
Robert Ross Wardrup <minorsecond@gmail.com>
2016-07-21 12:47:27 -0500

    Update test_calculations.py

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,11 +1,12 @@
 from gh import GisHelper
-GisHelper = GisHelper()
+dd_to_dms = GisHelper.dd_to_dms
+calculate_origin = GisHelper.calculate_origin
 
 def test_dd_to_dms():
-	assert GisHelper.dd_to_dms(38.898556) == '38d, 53m, 55s'
-	assert GisHelper.dd_to_dms(-77.037852) == '77d, 2m, 16s'
-	assert GisHelper.dd_to_dms(100.123456) == 'Invalid entry'
+	assert dd_to_dms(38.898556) == '38d, 53m, 55s'
+	assert dd_to_dms(-77.037852) == '77d, 2m, 16s'
+	assert dd_to_dms(100.123456) == 'Invalid entry'
 
 def test_origin_calculation():
 	coordinates = [(43.7291, 26.2946), (43.8058, 26.3759)]
-	assert GisHelper.calculate_origin(coordinates) == '43.76745, 26.33525'
+	assert calculate_origin(coordinates) == '43.76745, 26.33525'
