31ecd6d2e5faa18b42bed7fd64562b4601a1af1e
Ross Wardrup <minorsecond@gmail.com>
2016-07-30 10:08:19 -0500

    updated nose tests

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,6 +1,5 @@
 from gh import *
 
-
 def test_dd_to_dms():
     assert dd_to_dms(38.898556) == (38, 53, 54.802, True)
     assert dd_to_dms(-77.037852) == (-77, -2, 16.267, True)
@@ -11,5 +10,15 @@ def test_origin_calculation():
     coordinates = [0.0, 10.0, 0.0, 10.0]
     assert origin_calc(coordinates) == (5.0, 5.0)
 
-if __name__ == '__main__':
-    test_origin_calculation()
+
+def test_raster_bounds():
+    """
+    Tests raster bounding box calc
+    :return:
+    """
+
+    path = '/home/rwardrup/DEV/gis-helper/'
+    results = CalculateRasterBounds(path)
+    print(results)
+
+    assert results == (0, {})
