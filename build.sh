48b1427f772b96adc5c3cc585b77f059657c6724
Ross Wardrup <minorsecond@gmail.com>
2016-08-04 04:19:14 -0500

    added build script for Linux.

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# For use with WINE
+
+/home/rwardrup/DEV/venv_wine/bin/activate  # Activate the WINE virtual environment
+wine c:/Python34/python.exe /home/rwardrup/DEV/pyinstaller/pyinstaller.py /home/rwardrup/DEV/global-trigger-tool/assets/gh-debug.spec
