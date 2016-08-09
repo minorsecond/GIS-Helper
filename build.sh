6c713809138bf88681952529eaa5b564906f7ca5
Ross Wardrup <minorsecond@gmail.com>
2016-08-09 04:46:37 -0500

    Updated build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 # For use with WINE
 
 /home/rwardrup/DEV/venv_wine/bin/activate  # Activate the WINE virtual environment
-wine c:/Python34/python.exe /home/rwardrup/DEV/pyinstaller/pyinstaller.py /home/rwardrup/DEV/GIS-Helper/assets/gh-debug.spec
+wine c:/Python34/python.exe /home/rwardrup/DEV/pyinstaller/pyinstaller.py /home/rwardrup/DEV/GIS-Helper/gh-debug.spec
