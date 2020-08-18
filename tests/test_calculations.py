6969d6d062cd95dc42f0891b5510ff2eb1ad1943
Ross Wardrup <minorsecond@gmail.com>
2020-08-18 15:28:16 -0500

    GIS-Helper-34 Added comments.

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -3,12 +3,18 @@ from raster import measurements
 
 
 def test_dd_to_dms():
+    """
+    Test conversion from decimal degrees to degrees, minutes, seconds
+    """
     assert dd_to_dms(38.898556) == (38, 53, 54.802, True)
     assert dd_to_dms(-77.037852) == (-77, -2, 16.267, True)
     assert dd_to_dms(180.001) == (None, None, None, False)
 
 
 def test_origin_calculation():
+    """
+    Test orgin coordinate calculation.
+    """
     coordinates = [0.0, 10.0, 0.0, 10.0]
     assert origin_calc(coordinates) == (5.0, 5.0)
 
