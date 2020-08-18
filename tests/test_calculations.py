75b8409d3288c2513795948fcf58e769a845038a
Ross Wardrup <minorsecond@gmail.com>
2020-08-18 15:22:04 -0500

    GIS-Helper-34 Some cleanup.

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,6 +1,7 @@
 from gh import dd_to_dms, origin_calc
 from raster import measurements
 
+
 def test_dd_to_dms():
     assert dd_to_dms(38.898556) == (38, 53, 54.802, True)
     assert dd_to_dms(-77.037852) == (-77, -2, 16.267, True)
@@ -23,8 +24,8 @@ def test_raster_bounds():
     results = raster_measurements.CalculateRasterBounds(path)
     print(results)
 
-    assert results == (1, {'.\\tests\\test_data\\i30dem.tif': [532935.13,  # ulX
-                                               4205684.82, # ulY
-                                               577305.13,  # lrX
-                                               4149794.82  # lrY
-    ]})
+    assert results == (1, {'.\\tests\\test_data\\i30dem.tif': [532935.13,
+                                                               4205684.82,
+                                                               577305.13,
+                                                               4149794.82
+                                                               ]})
